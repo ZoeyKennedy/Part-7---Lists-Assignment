e1fe9cd94d1a695a3e290e5c121872d055a4c912
Zoey Kennedy <339523656@GoTVDSB.ca>
2024-04-17 14:21:40 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+
+namespace Part_7___Lists_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random Generator = new Random();
+
+
+
+            List<int> numbers = new List<int>() {};
+            for (int i = 1; i <= 20 ; i++) //20 numbers
+            {
+                numbers.Add(Generator.Next(10, 21));
+            }
+
+            foreach (int name in numbers)
+            {
+                Console.WriteLine();
+            }
+            foreach (int name in numbers)
+            {
+                Console.WriteLine($"{name}");
+            }
+
+
+
+
+
+
+
+        }
+    }
+}
